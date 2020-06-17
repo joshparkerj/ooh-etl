b769385357886de9c96349a57a82510cc08ffe9d
Josh <joshBBparker@gmail.com>
2020-06-16 23:23:44 -0700

    finish converting browser code for use in node

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-const fs = require('fs');
-const DOMParser = require('dom-parser');
-const xpath = require('xpath');
-const dom = require('xmldom').DOMParser;
 const { JSDOM } = require('jsdom');
 
-// const IN = require(`./${process.argv[2]}`);
-
-function xpathSelect(unparsedDocumentString, xpathSelector) {
-    const doc = new dom().parseFromString(unparsedDocumentString);
-    return xpath.select(xpathSelector, doc);
+function getDocument(htmlString) {
+    return new JSDOM(htmlString).window.document;
 }
 
-// mimeType should be text/html or text/xml
-function $x(unparsedDocumentString, mimeType, xpath) {
-    const domParser = new DOMParser();
-    const parsedDocument = domParser.parseFromString(unparsedDocumentString, mimeType);
-    const xpathResult = parsedDocument.evaluate(xpath, parsedDocument);
+function xpathSelect(dom, xpath) {
+    const xpathResult = dom.evaluate(xpath, dom, null, 0);
     const results = [];
     let result = xpathResult.iterateNext();
     while (result) {
@@ -31,48 +21,34 @@ function textElement(ancestor, elementName) {
 }
 
 function cdataXpath(ancestor, elementName, xpath) {
-    const domParser = new DOMParser();
-
-    ancestorDOM = JSDOM.fragment(ancestor.toString());
-
-    const htmlString = textElement(ancestorDOM, elementName);
-    const parsedDocument = domParser.parseFromString(htmlString, 'text/html');
-    const xpathResult = parsedDocument.evaluate(xpath, parsedDocument);
-    let match = xpathResult.iterateNext();
+    const htmlString = textElement(ancestor, elementName);
+    const parsedDocument = getDocument(htmlString);
+    const xpathResult = xpathSelect(parsedDocument, xpath);
     let innerText = '';
-    let numberOfElements = 0;
 
-    while (match) {
-        innerText += match.innerText;
-        match = xpathResult.iterateNext();
-        numberOfElements++;
-    }
+    xpathResult.forEach(result => {
+        innerText += result.textContent;
+    });
 
-    if (numberOfElements !== 1) {
-        console.log(`The number of elements found for elementName: ${elementName} xpath: ${xpath} was: ${numberOfElements}`);
+    if (xpathResult.length !== 1) {
+        console.log(`The number of elements found for elementName: ${elementName} xpath: ${xpath} was: ${xpathResult.length}`);
     }
 
     return innerText;
 }
 
 function cdataParser(ancestor, elementName, xpath, objectName, innerTextName, cb) {
-    const domParser = new DOMParser();
-
-    ancestorDOM = JSDOM.fragment(ancestor.toString());
-
-    const htmlString = textElement(ancestorDOM, elementName);
-    const parsedDocument = domParser.parseFromString(htmlString, 'text/html');
-    const xpathResult = parsedDocument.evaluate(xpath, parsedDocument);
-    let match = xpathResult.iterateNext();
+    const htmlString = textElement(ancestor, elementName);
+    const parsedDocument = getDocument(htmlString);
+    const xpathResult = xpathSelect(parsedDocument, xpath);
     let cdataParsed = {};
     let innerText = '';
     cdataParsed[objectName] = {};
 
-    while (match) {
-        innerText += match.innerText;
-        cdataParsed = cb(match, cdataParsed);
-        match = xpathResult.iterateNext();
-    }
+    xpathResult.forEach(result => {
+        innerText += result.textContent;
+        cdataParsed = cb(result, cdataParsed);
+    })
 
     cdataParsed[innerTextName] = innerText;
     return cdataParsed;
@@ -80,11 +56,11 @@ function cdataParser(ancestor, elementName, xpath, objectName, innerTextName, cb
 
 function payParser(ancestor, elementName, xpath) {
     let pay = cdataParser(ancestor, elementName, xpath, 'pay', 'payText', (match, pay) => {
-        let reMatch = match.innerText.match(/The median annual wage for (?<suboccupation>.+) was \$(?<pay>\d+,\d{3})/);
+        let reMatch = match.textContent.match(/The median annual wage for (?<suboccupation>.+) was \$(?<pay>\d+,\d{3})/);
         if (reMatch) {
             pay.pay[reMatch.groups.suboccupation] = +(+((reMatch.groups.pay.replace(',', '')) / 2080).toFixed(2));
         } else {
-            reMatch = match.innerText.match(/The median hourly wage for (?<suboccupation>.+) was \$(?<pay>\d+\.\d{2})/);
+            reMatch = match.textContent.match(/The median hourly wage for (?<suboccupation>.+) was \$(?<pay>\d+\.\d{2})/);
             if (reMatch) {
                 pay.pay[reMatch.groups.suboccupation] = Number(reMatch.groups.pay);
             }
@@ -102,38 +78,33 @@ function similarOccupationsParser(ancestor, elementName, xpath) {
             so.similarOccupation.list = [];
         }
 
-        so.similarOccupation.list.push(match.innerText.trim());
+        so.similarOccupation.list.push(match.textContent.trim());
 
         return so;
     });
 }
 
-let myFile = '';
-
-fs.readFile('./xml-compilation.xml', (err, data) => {
-    if (err) throw err;
-    myFile = data.toString();
-
-    const o = xpathSelect(myFile, '//occupation');
+function parseXmlCompilation(myDom) {
+    const o = xpathSelect(myDom, '//occupation');
 
     const r = o.map(occupation => {
         e = {};
-        occupationDOM = JSDOM.fragment(occupation.toString());
-        e.title = textElement(occupationDOM, 'title');
+        e.title = textElement(occupation, 'title');
         console.log(e.title);
         if (e.title === 'Military Careers') {
             return e;
         }
-        e.description = textElement(occupationDOM, 'description');
-        e.medianPayAnnual = +textElement(occupationDOM, 'qf_median_pay_annual value');
-        e.medianPayHourly = +textElement(occupationDOM, 'qf_median_pay_hourly value');
-        e.education = textElement(occupationDOM, 'qf_entry_level_education value');
-        e.workExperience = textElement(occupationDOM, 'qf_work_experience value');
-        e.training = textElement(occupationDOM, 'qf_on_the_job_training value');
-        e.numberOfJobs = textElement(occupationDOM, 'qf_number_of_jobs value');
-        e.employmentOutlook = textElement(occupationDOM, 'qf_employment_outlook description');
-        e.employmentOutlookCode = textElement(occupationDOM, 'qf_employment_outlook value');
-        e.projectedChangeInNumberOfJobs = textElement(occupationDOM, 'qf_employment_openings value');
+
+        e.description = textElement(occupation, 'description');
+        e.medianPayAnnual = +textElement(occupation, 'qf_median_pay_annual value');
+        e.medianPayHourly = +textElement(occupation, 'qf_median_pay_hourly value');
+        e.education = textElement(occupation, 'qf_entry_level_education value');
+        e.workExperience = textElement(occupation, 'qf_work_experience value');
+        e.training = textElement(occupation, 'qf_on_the_job_training value');
+        e.numberOfJobs = textElement(occupation, 'qf_number_of_jobs value');
+        e.employmentOutlook = textElement(occupation, 'qf_employment_outlook description');
+        e.employmentOutlookCode = textElement(occupation, 'qf_employment_outlook value');
+        e.projectedChangeInNumberOfJobs = textElement(occupation, 'qf_employment_openings value');
         e.whatTheyDo = cdataXpath(occupation, 'summary_what_they_do', '//p');
         e.howToBecomeOne = cdataXpath(occupation, 'summary_how_to_become_one', '//p');
         e.workEnvironment = cdataXpath(occupation, 'summary_work_environment', '//p');
@@ -149,4 +120,8 @@ fs.readFile('./xml-compilation.xml', (err, data) => {
     });
 
     r.forEach(e => console.log(`Job: ${e.title} Salary: ${e.medianPayAnnual} Growth Rating: ${e.employmentOutlookCode}`));
-});
+}
+
+JSDOM.fromFile('xml-compilation.xml')
+    .then(dom => dom.window.document)
+    .then(myDom => { parseXmlCompilation(myDom); });
